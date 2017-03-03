b5937b2ae2283823ba87cac55eece33390d6a769
Jason Ogasian <jogasian@book.com>
2017-03-02 16:06:09 -0800

    Added JQuery

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './script/components/App';
+import $ from 'jquery';
 import './style/dist/index.css';
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  $('#root')[0]
 );
